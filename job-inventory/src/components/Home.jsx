b41bfefb5520c5299a1f3225c3f64d01e97783fd
isratjahan <isratjahan2503@gmail.com>
2023-04-11 09:27:47 +0600

    rechart added

--- job-inventory/src/components/Home.jsx
+++ job-inventory/src/components/Home.jsx
@@ -2,15 +2,15 @@ import React from 'react';
 
 const Home = () => {
   return (
-    <div className='px-4 py-4 mx-auto mt-4 sm:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
-        <div>
+    <div className=' flex gap-x-20 px-4 py-4 mx-auto sm:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
+        <div className='mr-8 my-7'>
           <h1 className='text-6xl font-bold	tracking-wide'>One Step <br /> Closer To Your <br /> 
           <span className='text-blue-400'>Dream Job</span></h1>
           <p className='text-lg my-2.5'>Explore thousands of job opportunities with all the <br /> information you need. Its your future. Come find it. Manage all <br /> your job application from start to finish.</p>
           <button className='btn'>Get Started</button>
         </div>
         <div>
-          <img src="" alt="" />
+          <img className='h-96 mt-0' src="../src/assets/All Images/P3OLGJ1 copy 1.png" alt="" />
         </div>
     </div>
   );
--- job-inventory/src/components/Statistics.jsx
+++ job-inventory/src/components/Statistics.jsx
@@ -1,10 +1,63 @@
 import React from 'react';
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend,
+  ReferenceLine,
+  ResponsiveContainer,
+} from 'recharts';
 
 const Statistics = () => {
 
+  const data = [
+    {
+      id: 'assignment-1',
+      name: 'Build your new year new mission website',
+      marks: 60,
+    },
+    {
+      id: 'assignment-2',
+      name: 'Create a responsive G3-Architects website',
+      marks: 60,
+    },
+    {
+      id: 'assignment-3',
+      name: 'CSS Framework Ed Tech landing page',
+      marks: 56,
+    },
+    {
+      id: 'assignment-4',
+      name: 'Basic Javascript problem solving',
+      marks: 60,
+    },
+    {
+      id: 'assignment-5',
+      name: 'Geometry Genius',
+      marks: 60,
+    },
+    {
+      id: 'assignment-6',
+      name: 'API and ES6',
+      marks: 60,
+    },
+    
+  ];
   return (
     <div>
-        
+        <LineChart 
+        width={800}
+        height={500}
+        data={data}
+        >
+          <Line type="monotone" dataKey="marks" stroke="#8884d8" activeDot={{ r: 8 }} />
+          <XAxis dataKey="id" padding={{ left: 30, right: 30 }}></XAxis>
+          <YAxis></YAxis>
+          <Tooltip></Tooltip>
+        </LineChart>
     </div>
   );
 };
