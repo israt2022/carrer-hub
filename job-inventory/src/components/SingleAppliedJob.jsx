5b98a2ff7ee2ca8e0c4940124e6131dd5b326bd4
isratjahan <isratjahan2503@gmail.com>
2023-04-12 14:28:06 +0600

    applied jobs responsive update

--- job-inventory/src/components/SingleAppliedJob.jsx
+++ job-inventory/src/components/SingleAppliedJob.jsx
@@ -5,26 +5,26 @@ const SingleAppliedJob = ({jobData}) => {
     return (
         <div className="single-applied-job-container">
             <div className="flex flex-wrap">
-                <div className="job-info w-4/5 flex">
-                    <div className="job-company-logo">
+                <div className="job-info w-full lg:w-4/5 block text-center md:text-left md:flex">
+                    <div className="job-company-logo mx-auto md:mx-0 mb-5 md:mb-0">
                         <img src={jobData.company_logo} alt="" />
                     </div>
                     <div className="applied-job-data">
                         <div className="job-post-name">{jobData.job_post}</div>
                         <div className="job-company">{jobData.company}</div>
-                        <div className="job-types-container mt-4 flex">
+                        <div className="job-types-container mt-4 flex flex-wrap gap-4 justify-center md:justify-start">
                             <div className="job-mode">{jobData.job_mode}</div>
-                            <div className="job_type ml-4">{jobData.job_type}</div>
+                            <div className="job_type">{jobData.job_type}</div>
                         </div>
 
-                    <div className="job-location-and-salary flex mt-8">
+                    <div className="job-location-and-salary flex flex-wrap gap-4 mt-8 justify-center md:justify-start">
                         <div className="job-location"><img src="/assets/Icons/Location Icon.png" className="inline" alt="location pin" />{" "+ jobData.company_location}</div>
                         <div className="job-salary"><img src="/assets/Icons/Vector.png" className="inline" alt="currency" />{" "+ jobData.salary}</div>
                     </div>
                     </div>
                 </div>
-                <div className="applied-job-view-details w-1/5 flex items-center">
-                    <NavLink to={`/jobs/${jobData.id}`} className="btn">View Details</NavLink>
+                <div className="applied-job-view-details mt-20 lg:mt-0 mx-auto lg:w-1/5 block lg:flex lg:items-center lg:justify-center">
+                    <NavLink to={`/job/${jobData.id}`} className="view-details-btn">View Details</NavLink>
                 </div>
             </div>
         </div>
--- job-inventory/src/components/SingleJob.jsx
+++ job-inventory/src/components/SingleJob.jsx
@@ -8,12 +8,12 @@ const SingleJob = ({jobData}) => {
             <div className="job-post">{jobData.job_post}</div>
             <div className="job_company">{jobData.company}</div>
 
-            <div className="job-types flex">
+            <div className="job-types flex gap-4">
                 <div className="job-mode">{jobData.job_mode}</div>
                 <div className="job-type">{jobData.job_type}</div>
             </div>
 
-            <div className="job-location-and-salary flex">
+            <div className="job-location-and-salary flex gap-4">
                 <div className="job-location"><img src="/assets/Icons/Location Icon.png" className="inline" alt="location pin" />{" "+ jobData.company_location}</div>
                 <div className="job-salary"><img src="/assets/Icons/Vector.png" className="inline" alt="currency" />{" "+ jobData.salary}</div>
             </div>
