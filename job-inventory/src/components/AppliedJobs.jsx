a8043d08bf1c3bca1f204f90370250d3b46d3111
isratjahan <isratjahan2503@gmail.com>
2023-04-12 15:40:58 +0600

    filter functionality

--- job-inventory/src/components/AppliedJobs.jsx
+++ job-inventory/src/components/AppliedJobs.jsx
@@ -2,19 +2,43 @@ import React, { useEffect, useState } from 'react';
 import Header from './Header';
 import Footer from './Footer';
 import SingleAppliedJob from './SingleAppliedJob';
-import Filter from './Filter';
 
 const AppliedJobs = () => {
 
 
 const [jobs, setJobs] = useState([]);
+const [filteredJobs, setFilteredJobs] = useState([]);
+const [filter, setFilter] = useState("all");
 
+const filterJobs = (e) => {
+  setFilter(e.target.value);
+}
 
+// console.log(filter);
 
 useEffect(()=>{
   const appliedJobs = localStorage.getItem("appliedJobs");
   setJobs(JSON.parse(appliedJobs))
 },[])
+
+useEffect(()=>{
+  if(filter === "all"){
+    setFilteredJobs(jobs);
+
+  } else if(filter === "remote"){
+    const remoteJob = jobs.filter(job => job.job_mode === "Remote");
+    setFilteredJobs(remoteJob);
+  } else if(filter === "onsite"){
+    const onsiteJobs = jobs.filter(job => job.job_mode === "Onsite");
+    setFilteredJobs(onsiteJobs);
+  } else if(filter === "full-time"){
+    const fullTimeJobs = jobs.filter(job => job.job_type === "Full Time");
+    setFilteredJobs(fullTimeJobs);
+  } else if(filter === "part-time"){
+    const partTimeJobs = jobs.filter(job => job.job_type === "Part Time");
+    setFilteredJobs(partTimeJobs);
+  }
+},[jobs, filter])
 
   return (
     <div className='applied-jobs-container'>
@@ -23,9 +47,17 @@ useEffect(()=>{
                 <h2 className="page-title">Applied Jobs</h2>
             </div>  
         <div className="applied-jobs px-4 py-4 mx-auto sm:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8 my-20">
-          <Filter/>
+        <div className="filter-container flex justify-end mb-8">
+            <select id="filters" onChange={(e)=>filterJobs(e)} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500  p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
+            <option value="all">Filter by</option>
+            <option value="remote">Remote Job</option>
+            <option value="onsite">Onsite Job</option>
+            <option value="full-time">Full Time Job</option>
+            <option value="part-time">Part Time Job</option>
+            </select>
+        </div>
           {
-            jobs?.length === undefined ? <p>Not applied in any job yet!</p> : jobs?.map(job => <SingleAppliedJob key={job.id} jobData={job}/>)
+            filteredJobs?.length === undefined ? <p>Not applied in any job yet!</p> : filteredJobs?.map(job => <SingleAppliedJob key={job.id} jobData={job}/>)
           }
         </div>
 
